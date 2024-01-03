5281890350029197225936e6f8f12c9c1a06f2d7
ccollards19 <cy.collard@gmail.com>
2024-01-03 22:54:53 +0100

    clean start

--- transcendence/transcendence/urls.py
+++ transcendence/transcendence/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for transcendance project.
+URL configuration for transcendence project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,6 +18,5 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("home/", include("home.urls")),
     path('admin/', admin.site.urls),
 ]
