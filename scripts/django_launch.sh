f095463aea52e64779355d2b84c73802f228fb0f
ccollards19 <cy.collard@gmail.com>
2024-03-04 13:40:01 +0100

    api start and new branch

--- scripts/django_launch.sh
+++ scripts/django_launch.sh
@@ -1,4 +1,4 @@
 python3 check_db.py;
-python3 transcendence/manage.py makemigrations;
-python3 transcendence/manage.py migrate;
-python3 transcendence/manage.py runserver 0.0.0.0:8000;
+python3 backend/manage.py makemigrations;
+python3 backend/manage.py migrate;
+python3 backend/manage.py runserver 0.0.0.0:8000;
