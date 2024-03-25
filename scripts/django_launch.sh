b172a2e343567d2ca384b6c4d124ef1ec7e9434e
ccollards19 <cy.collard@gmail.com>
2024-03-25 12:32:38 +0100

    compose fixed

--- scripts/django_launch.sh
+++ scripts/django_launch.sh
@@ -1,4 +1,4 @@
-python3 check_db.py;
-python3 backend/manage.py makemigrations;
-python3 backend/manage.py migrate;
-python3 backend/manage.py runserver 0.0.0.0:8000;
+python3 /check_db.py;
+python3 /usr/src/app/backend/manage.py makemigrations;
+python3 /usr/src/app/backend/manage.py migrate;
+python3 /usr/src/app/backend/manage.py runserver 0.0.0.0:8000;
