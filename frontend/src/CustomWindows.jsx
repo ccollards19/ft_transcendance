c9c890df489b22eeb2aa6ae05ccffdc740586a53
Gilles Poncelet <gilles.ponpon@gmail.com>
2024-03-25 14:32:10 +0100

    update

--- frontend/src/CustomWindows.jsx
+++ frontend/src/CustomWindows.jsx
@@ -460,7 +460,7 @@ export function Play({props}) {
 
 	return (
 		<div style={props.customwindow}>
-			{props.myProfile && props.scope === 'remote' ?
+			{props.myProfile && props.settings.scope === 'remote' ?
 				<Remote props={props} /> :
 				<Local props={props} />
 			}
