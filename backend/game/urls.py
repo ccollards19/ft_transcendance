a83bd49dbaa1ea52669e74bcc7a02786a37399ab
Nicolas Espana ribera <niespana@fu-r7-p2.s19.be>
2024-03-26 17:29:26 +0100

    nicodjango

--- backend/game/urls.py
+++ backend/game/urls.py
@@ -1,10 +1,11 @@
 # urls.py
 from django.urls import path
 from .views import RoomDetail, RoomCreate, AddPlayer
-from .views import PostChessMove
+from .views import PostChessMove, ChessMoves
 urlpatterns = [
     path('room/create/', RoomCreate.as_view(), name='room-create'),
     path('room/<str:room_id>/', RoomDetail.as_view(), name='room-detail'),
     path('room/<str:room_id>/add-player/<str:player_id>/', AddPlayer.as_view(), name='add-player'),
     path('post', PostChessMove, name='post-chess-move'),
+    path('chess/moves/<str:room_id>/', ChessMoves.as_view(), name='chess-moves')
 ]
