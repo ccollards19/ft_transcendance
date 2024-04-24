1cec9e6c441141cb58e353b4288ba1aa7324e503
Zapratkyn <gilles.ponpon@gmail.com>
2024-04-24 14:32:30 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,7 +56,10 @@ function WebSite() {
 		}
 		if (socket) {
 			socket.onopen = () => setChats(chats.map(chat => { return {...chat, messages : chat.messages.filter(message => message.type !== 'error')} }))
-			socket.onerror = () => setChats(chats.map(chat => { return {...chat, messages : [...chat.messages, {type : 'error'}]} }))
+			socket.onerror = () => {
+				setChats(chats.map(chat => { return {...chat, messages : [...chat.messages, {type : 'error'}]} }))
+				socket.error = true
+			}
 			socket.onclose = () => setChats(chats.map(chat => { return {...chat, messages : [...chat.messages, {type : 'error'}]} }))
 			socket.onMyProfile = data => setMyProfile(data)
 			socket.onChat = data => {
@@ -67,11 +70,14 @@ function WebSite() {
 						return chat
 					}))
 			}
-			// const interval = setInterval(() => {
-			// 	if (socket.readyState === 3 || socket.readyState === 0)
-			// 		setSocket(new WebSocket('ws://localhost/ws/'))
-			// }, 5000)
-			// return () => clearInterval(interval)
+			const interval = setInterval(() => {
+				if (socket.error) {
+					let sock = new WebSocket('ws://localhost/ws/')
+					sock.id = socket.id 
+					setSocket(sock)
+				}
+			}, 5000)
+			return () => clearInterval(interval)
 		}
 	}, [chats, socket, request])
 
