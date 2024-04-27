a54e6c66d6794a2c24cf49c3f2fd39d2615b391c
Zapratkyn <gilles.ponpon@gmail.com>
2024-04-27 16:35:45 +0200

    update

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -204,7 +204,7 @@ export default function Profile({props}) {
                     	        Nothing to display... Yet
                     	    </div> :
 							<ul className="d-flex rounded w-100 list-group overflow-auto noScrollBar" style={{minHeight: '300px', maxWidth: '280px'}}>
-							{requests.map(request => { return <Request key={index++} props={props} profile={request.item} id={request.id} requests={requests} setRequests={setRequests} /> }) && 
+							{requests && requests.map(request => { return <Request key={index++} props={props} profile={request.item} id={request.id} requests={requests} setRequests={setRequests} /> }) && 
 							friends && friends.map(friend => {
 								if (friend.item.status === 'online')
 									return <Friend key={index++} props={props} profile={friend.item} id={friend.id} setDisplay={setDisplay} />
