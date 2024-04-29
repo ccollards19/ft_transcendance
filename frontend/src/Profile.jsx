48d2d37b83e7bec436d3e848668750d3a9a2b871
Gilles Poncelet <gponcele@fu-r7-p1.s19.be>
2024-04-29 13:48:41 +0200

    update

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -209,13 +209,13 @@ export default function Profile({props}) {
                     	    </div> :
 							<ul className="d-flex rounded w-100 list-group overflow-auto noScrollBar" style={{minHeight: '300px', maxWidth: '280px'}}>
 							{requests.map(request => { return <Request key={index++} props={props} profile={request.item} id={request.id} requests={requests} setRequests={setRequests} /> }).concat(
-							friends && friends.map(friend => {
+							friends.map(friend => {
 								if (friend.item.status === 'online')
 									return <Friend key={index++} props={props} profile={friend.item} id={friend.id} setDisplay={setDisplay} />
 								else
 									return undefined
 							})).concat(
-								friends && friends.map(friend => {
+								friends.map(friend => {
 									if (friend.item.status === 'offline')
 										return <Friend key={index++} props={props} profile={friend.item} id={friend.id} setDisplay={setDisplay} />
 									else
