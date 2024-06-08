81058c25560e93a9f2991459857d38c0be9c27de
Zapratkyn <gilles.ponpon@gmail.com>
2024-06-08 13:54:27 +0200

    update

--- backend/tournaments/urls.py
+++ backend/tournaments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from tournaments.views import TournamentCreate, SetTournamentImages, GetTournament, GetAllTournaments, Subscribe, UpdateDescription
+from tournaments.views import TournamentCreate, SetTournamentImages, GetTournament, GetAllTournaments, Subscribe, UpdateDescription, UpdateImage
 
 urlpatterns = [
     path('create/', TournamentCreate.as_view(), name="tournament-create"),
     path('<int:id>/setImages/', SetTournamentImages.as_view(), name='tournament-update-images'),
+    path('<int:id>/updateImage/', UpdateImage.as_view()),
     path("<int:id>/", GetTournament.as_view()),
     path("all/<str:game>/", GetAllTournaments.as_view()),
     path("subscribe/<int:id>/", Subscribe.as_view()),
